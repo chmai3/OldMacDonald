de1849554a65d7d9afe11dcb42c400349c1274e8
chmai3 <36012557+chmai3@users.noreply.github.com>
2018-10-30 14:18:56 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,8 +1,6 @@
 class NamedCow
 {
   private String myName;
-  private String myType;
-  private String mySound;
   public NamedCow( String name, String type, String sound)
   {
     myName = name;
@@ -12,13 +10,5 @@ class NamedCow
   public String getName()
   {
     return myName;
-  }
-  public String getType()
-  {
-    return myType;
-  }
-  public String getSound()
-  {
-    return mySound;
   }
 }
